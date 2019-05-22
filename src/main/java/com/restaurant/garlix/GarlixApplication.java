9f025a91acbd7edb6c6deedd221c10cac6951e62
Abdulhakim Alsharif <47099709+Abalsh@users.noreply.github.com>
2019-05-23 00:57:50 +0300

    Initial Setup

---
+++ src/main/java/com/restaurant/garlix/GarlixApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.garlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarlixApplication.class, args);
+    }
+
+}
