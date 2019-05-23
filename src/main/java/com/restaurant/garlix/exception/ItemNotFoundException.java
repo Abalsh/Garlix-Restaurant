185b6866e152361e2f47eb6fff028098c5722a21
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-05-24 00:14:21 +0300

    Add ItemNotFound Exception

--- src/main/java/com/restaurant/garlix/exception/ItemNotFoundException.java
+++ src/main/java/com/restaurant/garlix/exception/ItemNotFoundException.java
@@ -1,4 +1,29 @@
 package com.restaurant.garlix.exception;
 
-public class ItemNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ItemNotFoundException extends RuntimeException{
+    private String itemName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ItemNotFoundException(String itemName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", itemName, fieldName, fieldValue));
+        this.itemName = itemName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
 }
