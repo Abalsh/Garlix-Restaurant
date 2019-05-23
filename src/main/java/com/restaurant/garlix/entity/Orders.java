20d49937d423725b037881ebd99d74159ec6c30b
Florian <florian.shllaku@gmail.com>
2019-05-23 03:39:31 +0300

    add not null to varchar columns

--- src/main/java/com/restaurant/garlix/entity/Orders.java
+++ src/main/java/com/restaurant/garlix/entity/Orders.java
@@ -1,55 +1,56 @@
 package com.restaurant.garlix.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
-public class Order {
+public class Orders {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private long ID;
+    private long id;
+    @NotNull
     private String Client_name;
+    @NotNull
     private String Client_address;
+
     private int Total_price;
     private boolean takeaway = true;
 
-    public long getID() {
-        return ID;
-    }
-
-    public void setID(long ID) {
-        this.ID = ID;
+    public long getId() {
+        return id;
     }
 
     public String getClient_name() {
         return Client_name;
     }
 
-    public void setClient_name(String client_name) {
-        Client_name = client_name;
-    }
-
     public String getClient_address() {
         return Client_address;
-    }
-
-    public void setClient_address(String client_address) {
-        Client_address = client_address;
     }
 
     public int getTotal_price() {
         return Total_price;
     }
 
-    public void setTotal_price(int total_price) {
-        Total_price = total_price;
-    }
-
     public boolean isTakeaway() {
         return takeaway;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setClient_name(String client_name) {
+        Client_name = client_name;
+    }
+
+    public void setClient_address(String client_address) {
+        Client_address = client_address;
+    }
+
+    public void setTotal_price(int total_price) {
+        Total_price = total_price;
     }
 
     public void setTakeaway(boolean takeaway) {
