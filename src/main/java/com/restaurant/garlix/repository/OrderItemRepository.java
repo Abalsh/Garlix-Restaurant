bb061d02a110cd65488cffffb7dbb27aabfa934c
shibra <shibra@taltech.ee>
2019-05-25 03:30:38 +0300

    Create repositories for Orders and OrdersItem classes

---
+++ src/main/java/com/restaurant/garlix/repository/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.restaurant.garlix.repository;
+
+import com.restaurant.garlix.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
+
+}
